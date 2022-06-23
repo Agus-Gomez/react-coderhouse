bbb643bf298614f1796799760d7643aac528a8fb
Agustina Alejandra Gómez <agusgomez787@gmail.com>
2022-06-23 18:59:13 -0300

    Desafío 6 de card con pantalla de carga y botón a más info funcional

--- src/components/ItemDetailContainer/itemDetailContainer.js
+++ src/components/ItemDetailContainer/itemDetailContainer.js
@@ -4,18 +4,30 @@ import { getFetch } from "../Fetch/Fetch";
 import { useParams } from "react-router-dom";
 
 const ItemDetailContainer = () => {
-  const [product, setProduct] = useState({})
-
-  const { id } = useParams()
-
+  const [product, setProduct] = useState({});
+  const [loading, setLoading] = useState(true);   
+  const { id } = useParams();
 
   useEffect(() => {
     getFetch(id)
-      .then((resp) => setProduct(resp))
+      .then((resp) => {
+         setProduct(resp)
+         setLoading(false)})
       .catch((err) => setProduct(err));
-  }, );
+  });
 
-
-  return <ItemDetail product={product}/>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Cargando información del producto ...</h1>
+      ) : (
+        <div
+          style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}
+        >
+          <ItemDetail product={product} />{" "}
+        </div>
+      )}
+    </div>
+  );
 };
 export default ItemDetailContainer;
