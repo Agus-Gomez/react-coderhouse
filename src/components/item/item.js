de350b768ad02bfbcf13326eee75c047f10a5486
Agustina Alejandra Gomez <agusgomez787@gmail.com>
2022-08-03 19:16:28 -0300

    arreglo de enlaces

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -3,11 +3,14 @@ import "./Item.css";
 
 const Item = ({ prod }) => {
   return (
+
     <div className="cardItem mt-5 mb-2">
       <div className="card-header">{`${prod.Name}`}</div>
+
       <div className="card-body">
         <img src={prod.Pic} alt="" />
       </div>
+      
       <div className="card-footer">
         <Link to={`/ItemDetail/${prod.id}`}>
           <button>detalle del producto</button>
